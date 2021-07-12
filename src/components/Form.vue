6a9c1555ceaf8531c770ac5037537c3cacce958b
Igor <info@pro-landing.site>
2021-07-12 12:26:31 +0300

    Создал компонент формы

---
+++ src/components/Form.vue
@@ -0,0 +1,41 @@
+<template>
+   <form class="sign-up">
+      <div class="form-group">
+         <label for="login">Логин:</label>
+         <input class="form-control" id="login" type="text">
+      </div>
+      <div class="form-group">
+         <label for="email">Почта:</label>
+         <input class="form-control" id="email" type="text">
+      </div>
+      <div class="form-group">
+         <label for="phone">Телефон:</label>
+         <input id="phone" class="form-control" type="text">
+      </div>
+
+      <div>
+         
+      </div>
+   </form>
+</template>
+
+<script>
+   export default {
+      
+   }
+</script>
+
+<style scoped>
+.sign-up{
+   margin: 50px
+}
+.form-control{
+   width: 400px;
+}
+.form-group{
+   margin-bottom: 15px;
+}
+label{
+   margin-bottom: 5px;
+}
+</style>
