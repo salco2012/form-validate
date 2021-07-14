b186fdeb2efa7250ead77fb1a8a9fe8075331038
Igor <info@pro-landing.site>
2021-07-15 00:33:27 +0300

    добавил валидацию на инпуты

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,10 +3,12 @@
       <div class="form-group">
          <label for="name">Имя:</label>
          <input class="form-control" id="name" type="text" v-model.trim="form.name">
+         <p class="error" v-if="$v.form.name.$dirty && !$v.form.name.alpha">Только буквы</p>
       </div>
       <div class="form-group">
          <label for="full-name">Фамилия:</label>
          <input class="form-control" id="full-name" type="text" v-model.trim="form.fullName">
+         <p class="error" v-if="$v.form.fullName.$dirty && !$v.form.fullName.alpha">Только буквы</p>
       </div>
       <div class="form-group">
       <label for="сurrent-city">Город проживания:</label>
@@ -29,22 +31,30 @@
                   {{ profession.label }}
                </option>
          </select>
+         <p class="error" v-if="$v.form.profession.$dirty && !$v.form.profession.maxCount">Не более 3-х вариантов</p>
       </div>
             <div class="form-group">
          <label for="email">Почта:</label>
          <input  id="email" class="form-control" type="email" v-model.trim="form.email">
+         <p class="error" v-if="$v.form.email.$dirty && !$v.form.email.required">Обязательное поле</p>
+         <p class="error" v-if="$v.form.email.$dirty && !$v.form.email.email">Это не похоже на email</p>
       </div>
       <div class="form-group">
          <label for="phone">Контактный телефон:</label>
          <input class="form-control" id="phone" type="text" v-model.trim="form.phone">
+         <p class="error" v-if="$v.form.phone.$dirty && !$v.form.phone.numeric">Только цифры</p>
       </div>
       <div class="form-group">
          <label for="login">Логин</label>
          <input class="form-control" id="login" type="text" v-model.trim="form.login">
+         <p class="error" v-if="$v.form.login.$dirty && !$v.form.login.required">Обязательное поле</p>
+         <p class="error" v-if="$v.form.login.$dirty && !$v.form.login.alpha">Только латиница</p>
       </div>
          <div class="form-group">
          <label for="password">Пароль</label>
          <input class="form-control" id="password" type="password" v-model.trim="form.password">
+         <p class="error" v-if="$v.form.password.$dirty && !$v.form.password.required">Обязательное поле</p>
+         <p class="error" v-if="$v.form.password.$dirty && !$v.form.password.minLength">Введите минимум 6 символов</p>
       </div>
       <button type="submit" class="btn btn-primary">Зарегистрироваться</button>
    </form>
@@ -52,6 +62,8 @@
 
 <script>
 import { validationMixin } from 'vuelidate'
+import { required, minLength, email, numeric, helpers} from 'vuelidate/lib/validators'
+const alpha = helpers.regex('alpha', /^[a-zA-Z]*$/)
 
    export default {
       mixins: [validationMixin],
@@ -130,12 +142,38 @@ import { validationMixin } from 'vuelidate'
       },
       methods: {
          onSubmirForm() {
-
+             this.$v.form.$touch()
          }
       },
       validations: {
          form: {
-            
+            name: {
+               alpha
+            },
+            fullName: {
+               alpha
+            },
+            email: {
+               minLength: minLength(4),
+               required,
+               email
+            },
+            phone: {
+               numeric
+            },
+            login: {
+               required,
+               alpha
+            },
+            password: {
+               minLength: minLength(6),
+               required,
+            },
+            profession: {
+               maxCount(value) {
+                  return value.length <= 3
+               }
+            }
          }
       }
    }
@@ -154,5 +192,8 @@ import { validationMixin } from 'vuelidate'
 }
 label{
    margin-bottom: 5px;
+}
+.error{
+   color: red;
 }
 </style>
